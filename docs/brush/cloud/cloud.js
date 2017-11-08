7d93bd331241dc9681b76c3ed114e5026be704e4
dags <dags-@users.noreply.github.com>
2017-11-08 13:18:01 +0000

    huge performance boost
    for the purposes of displaying the section, only need to calculate the dy alpha values when z = 0 (rather than calculating all positions and only drawing those that were on z = 0)

--- docs/brush/cloud/cloud.js
+++ docs/brush/cloud/cloud.js
@@ -1,65 +1,58 @@
-let brush = new Brush(perform);
+let shape = new Circle();
 let octaves = 3;
 let frequency = 0.01;
 
 let height = 16;
+let heightRange = (2 * height) + 1;
 let offset = 4;
 let detail = 4;
 let density = 0.5;
+let center = 0.5;
 
-let max = 1;
+let maxNoise = 1;
 let range = 255;
-let emptiness = 0;
+let air = 0;
+let bitrate = 15;
 
 let xOff = 0;
 let zOff = 0;
+
+function draw(buffer) {
+  shape.iterate(perform);
+}
 
 function perform(buffer, x, z, dist2) {
   let px = x + xOff;
   let pz = z + zOff;
 
-  let hmod = mod(dist2, brush.radius2);
-  let opac = opacity(dist2, brush.featherRadius, brush.featherRange);
-  let noise0 = getValue(px, 0, pz, frequency, octaves) / max;
-  let elevation = (noise0 * height * hmod);
+  let hmod = mod(dist2, shape.radius2);
+  let noise0 = getValue(px, 0, pz, frequency, octaves) / maxNoise;
+  let elevation = (noise0 * heightRange * hmod);
 
-  let startY = -offset;
-  let endY = offset + elevation;
+  let startY = -Math.round(elevation * center);
+  let endY = startY + elevation;
   let sectionY = offset > 1 ? -1 : 0;
+  let feather = getFeather(dist2, shape.featherRadius, shape.featherRange);
 
-  for (let dy = startY; dy <= endY; dy++) {
-    let denom = dy < 0 ? Math.min(-1, startY - 1) : Math.max(1, endY + 1);
-    let vmod = mod(dy, denom);
-    let noise1 = getValue(px * detail, dy, pz * detail, frequency, octaves) / max;
-    let alpha = (range * noise1 * opac * vmod) - emptiness;
-    
-    alpha = clampAlpha(alpha * 1.5, 15); // clamps the alpha to an integer value 0-15. the x1.5 just brightens it up a little
+  let planAlpha = getAlpha(px, 0, pz, startY, endY, feather);
+  buffer.drawPlan(x, z, 255, 255, 255, planAlpha);
 
-    if (dy === sectionY) {
-      buffer.setPlan(x, z, 255, 255, 255, alpha);
-    }
-
-    if (z === buffer.planCenterY) {
-      buffer.setSection(x, buffer.sectionCenterY - dy, 255, 255, 255, alpha);
+  if (z === 0) {
+    for (let dy = startY; dy <= endY; dy++) {
+      let sectionAlpha = getAlpha(px, dy, pz, startY, endY, feather);
+      buffer.drawSection(x, -dy, 255, 255, 255, sectionAlpha);
     }
   }
 }
 
-function updatePos(x, z) {
-  brush.update();
-  max = maxValue(octaves);
-  emptiness = 255 * (1.0 - density);
-  range = 255 + emptiness;
-  xOff = x;
-  zOff = z;
+function getAlpha(x, y, z, mn, mx, feather) {
+  let vmod = mod(y, y < 0.5 ? Math.min(-1, mn) : Math.max(1, mx));
+  let noise = getValue(x * detail, y, z * detail, frequency, octaves) / maxNoise;
+  let alpha = (range * noise * feather * vmod) - air;
+  return clampAlpha(alpha * 1.5, bitrate);
 }
 
-function mod(val, range) {
-  let mod = val / range;
-  return 1 - (mod * mod);
-}
-
-function opacity(val, bound, range) {
+function getFeather(val, bound, range) {
   if (val > bound) {
     let d = val - bound;
     return 1 - (d / range);
@@ -73,32 +66,39 @@ function clampAlpha(value, bitrate) {
     return Math.min(255, Math.max(0, steps * Math.round(255 / bitrate)));
 }
 
-function register(controls) {
-  controls.appendChild(slider('scale', 1, 96, 64, 1, function(val) {
-    frequency = 1 /val;
-  }));
-  controls.appendChild(slider('octaves', 1, 6, 4, 1, function(val) {
+function mod(val, range) {
+  let mod = val / range;
+  return 1 - (mod * mod);
+}
+
+function setPos(x, z) {
+  xOff = x;
+  zOff = z;
+}
+
+function registerOptions(register) {
+  register(createSlider('scale', 1, 96, 64, 1, (val) => frequency = 1 / val));
+  register(createSlider('octaves', 1, 6, 4, 1, (val) => {
     octaves = val;
+    maxNoise = maxValue(octaves);
   }));
-  controls.appendChild(slider('radius', 8, 58, 45, 1, function(val) {
-    brush.radius = val;
-  }));
-  controls.appendChild(slider('feather', 0, 100, 50, 100, function(val) {
-    brush.feather = val;
-  }));
-  controls.appendChild(slider('density', 0, 100, 85, 100, function(val) {
+  register(createSlider('radius', 8, 70, 55, 1, (val) => shape.radius = val));
+  register(createSlider('feather', 0, 100, 50, 100, (val) => shape.feather = val));
+  register(createSlider('density', 0, 100, 85, 100, (val) => {
     density = val;
+    air = 255 * (1.0 - density);
+    range = 255 + air;
   }));
-  controls.appendChild(slider('detail', 10, 50, 15, 10, function(val) {
-    detail = val;
-  }));
-  controls.appendChild(slider('height', 4, 48, 20, 1, function(val) {
+  register(createSlider('detail', 10, 50, 17, 10, (val) => detail = val));
+  register(createSlider('height', 4, 48, 20, 1, (val) => {
     height = val;
+    heightRange = (2 * height) + 1;
   }));
-  controls.appendChild(slider('y-offset', 0, 16, 3, 1, function(val) {
-    offset = val;
-  }));
-  controls.appendChild(slider('speed', -10, 10, 1, 10, function(val) {
-    increment = val;
-  }));
+  register(createSlider('center', 0, 100, 30, 100, (val) => center = val));
+  register(createSlider('bit-depth', 1, 32, 15, 1, (val) => bitrate = val));
+}
+
+function writeOutput(output) {
+  let scale = Math.round(1 / frequency);
+  output.value = `/set ${scale};${octaves};${shape.radius};${shape.feather};${density};${detail};${height};${center}`;
 }
